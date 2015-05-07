4d0e5af72dc03ec5b61edbbb3143a90483d5a6ae
Claudia Cuevas <omordn@gmail.com>
2015-05-07 09:06:06 -0700

    add working delete route with successful mocha tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,34 @@
 'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      dev: {
+        src: ['*.js', '/models/**/*.js', '/routes/**/*.js', '/test/**/*.js']
+      },
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    simplemocha: {
+      dev: {
+        src: ['/test/**/*.js']
+      }
+    },
+    watch: {
+      app: {
+        files: ['<%= jshint.dev.src %>']
+        tasks: ['jshint', 'simplemocha']
+      }
+    }
+  });//end grunt initConfig
+
+  grunt.registerTest('test', ['jshint:dev']);
+  grunt.registerTest('mocha', ['simeplemocha:dev']);
+  grunt.registerTest('default', ['test', 'mocha', 'watch']);
+};
--- test/pet_api_test.js
+++ test/pet_api_test.js
@@ -1,1 +1,41 @@
 'use strict';
+
+//db for testing purposes
+process.env.MONGOLAB_URI = 'mongodb://localhost/pet_test';
+require('../server.js');
+
+var mongoose = require('mongoose');
+var chai = require('chai');
+var chaihttp = require('chai-http');
+chai.use(chaihttp);
+var expect = chai.expect;
+
+var Pet = require('../models/Pet.js');
+
+describe('modify existing Pet documents', function() {
+  beforeEach(function(done) {
+    var petTest = new Pet({name: 'Peanut', owner: 'Claudia', weight: 22, type: 'dog'});
+    petTest.save(function(err, data) {
+      if(err) throw err;
+
+      this.petTest = data;
+      done();
+    }.bind(this));//end Pet save
+  });//end before each
+
+  it('should verify that before each creates a Pet', function() {
+    expect(this.petTest.name).to.eql('Peanut');
+    expect(this.petTest).to.have.property('_id');
+  });
+
+  it('should DELETE a pet collection', function(done) {
+    chai.request('localhost:3000')
+      .del('/api/pets/' + this.petTest._id)
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        console.log(res.body.msg);
+        expect(res.body.msg).to.equal('success');
+        done();
+      });
+  });//end DELETE test
+});
